90d57dd03d214eb95987cbe322fd18e7c4247c60
javierhuebra <javieremanuelhuebra@gmail.com>
2022-08-28 20:20:56 -0300

    :sparkles: feat:adding testigo button

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
 let llamada = new Audio('./audio/llamada.mp3');
 let cortina = new Audio('./audio/cortina.mp3');
-
+let testigo = new Audio('./audio/testimonio.mp3');
+let sonando=false;
 
 btnLlamada=document.querySelector(".llamada");
 btnCortina=document.querySelector(".cortina");
+btnTestigo=document.querySelector(".testigo");
+
 
 btnLlamada.addEventListener('click', () =>{
     console.log("Boton de llamada presionado");
-    btnLlamada.style.backgroundColor = 'yellowgreen';
-    llamada.play();
+    if(sonando==false){
+        sonando=true;
+        btnLlamada.style.backgroundColor = 'yellowgreen';
+        llamada.play();
+    }else{
+        llamada.pause();
+        llamada.currentTime=0;
+        sonando=false;
+        btnLlamada.style.backgroundColor = '#074C61';
+    
+    }
+    
     
     llamada.addEventListener("ended",()=>{
         btnLlamada.style.backgroundColor = '#074C61';
@@ -17,13 +30,42 @@ btnLlamada.addEventListener('click', () =>{
 
 btnCortina.addEventListener('click', () =>{
     console.log("Boton de cortina presionado");
-    btnCortina.style.backgroundColor = 'yellowgreen';
-    cortina.play();
+    if(sonando==false){
+        sonando=true;
+        btnCortina.style.backgroundColor = 'yellowgreen';
+        cortina.play();
+    }else{
+        cortina.pause();
+        cortina.currentTime=0;
+        sonando=false;
+        btnCortina.style.backgroundColor = '#074C61';
+    
+    }
     
     cortina.addEventListener("ended",()=>{
         btnCortina.style.backgroundColor = '#074C61';
     });
 });
+
+
+ btnTestigo.addEventListener('click', () =>{
+    console.log("Boton de cortina presionado");
+    if(sonando==false){
+        sonando=true;
+        btnTestigo.style.backgroundColor = 'yellowgreen';
+        testigo.play();
+    }else{
+        testigo.pause();
+        testigo.currentTime=0;
+        sonando=false;
+        btnTestigo.style.backgroundColor = '#074C61';
+    
+    }
+    
+    cortina.addEventListener("ended",()=>{
+        btnCortina.style.backgroundColor = '#074C61';
+    });
+}); 
 
 window.onload = () => {
     document.querySelector(".container").style.opacity='1';
